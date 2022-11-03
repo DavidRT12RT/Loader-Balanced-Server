7ef8f57264e18df616d0a11d1c8077176a9ce8c2
David Melgarejo <davidarcosm@hotmail.com>
2022-11-03 10:22:00 -0600

    Loader balanced with proxies for university proyect done

--- app.js
+++ app.js
@@ -1,34 +1,59 @@
-const express = require("express");
 require('dotenv').config()
 //const app = express();
 const port = process.env.PORT; //80
 const proxy = require('express-http-proxy');
 const app = require('express')();
+const fs = require("fs");
 
 
+//Pure functions!!!
 
-app.get("/home",proxy("192.168.100.20:4000/home",{
-    forwardPath: function (req, res) {
-      console.log("Llego peticion!");
-      console.log("Redirigendo!");
-      return '/home' 
-    }
+
+const getRandomNumber = (max) => {
+    return Math.floor(Math.random() * max);
+}
+
+
+function selectHost(){
+    const rawData = fs.readFileSync("servers.json");
+    const servidores = JSON.parse(rawData);
+    const serversLength = Object.keys(servidores).length;
+    //Ningun webserver registrado aun!
+    if( serversLength == 0) return reject("Ningun webfarm registrado aun!");
+    const serverChoice = getRandomNumber(serversLength);
+    const servidor = servidores[serverChoice];
+	console.log(servidor.ip);
+	return `${servidor.ip}:4000`;
+}
+
+
+app.get("/",proxy(selectHost,{
+	proxyReqPathResolver:function (req,res) {
+		return "/"
+	}
+}));
+
+app.get("/home",proxy(selectHost,{
+	proxyReqPathResolver:function (req,res) {
+		return "/home"
+	}
+}));
+
+app.get("/about",proxy(selectHost,{
+	proxyReqPathResolver:function (req,res) {
+		return "/about"
+	}
+}));
+
+app.get("/contact",proxy(selectHost,{
+	proxyReqPathResolver:function (req,res) {
+		return "/contact"
+	}
 }));
 
 
-app.get("/about",proxy("www.google.com",{
-    forwardPath: function (req, res) {
-      return '/' 
-    }
-}));
-
-app.get("/contact",proxy("www.google.com",{
-    forwardPath: function (req, res) {
-      return '/' 
-    }
-}));
 
 
 app.listen(port,()=>{
-    console.log(`Loader balancer running on port ${port}`);
+	console.log(`Loader balancer running on port ${port}`);
 });
